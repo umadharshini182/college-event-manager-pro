ff87d9ffcef18ee8ba52c24ebd2302029258253e
Uma Dharshini <yourgithubemail@example.com>
2026-07-15 00:02:51 +0530

    Protect admin dashboard and responsive fixes

--- admin.js
+++ admin.js
@@ -379,16 +379,3 @@ document
 };
 
 }
-const menuBtn = document.getElementById("menuBtn");
-
-if(menuBtn){
-
-menuBtn.onclick = function(){
-
-document
-.querySelector(".sidebar")
-.classList.toggle("active");
-
-};
-
-}
