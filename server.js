e507d9b93a098eb6a1eefd7885167a27eb86a237
Uma Dharshini <yourgithubemail@example.com>
2026-07-19 01:16:14 +0530

    Remove .env from repository

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const path = require("path");
 const mysql = require("mysql2");
+const Razorpay = require("razorpay");
 
 const app = express();
+const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+});
 
 app.use(cors({
     origin: true,
